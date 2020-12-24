2e24002b1f457c1a591fc6f698cef0b05632a5ae
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-12-24 05:57:17 +0530

    setup postgresql as db

--- config/database.js
+++ config/database.js
@@ -1,14 +1,23 @@
+const db_uri_parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        username: config.username,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true
       },
     },
   },
